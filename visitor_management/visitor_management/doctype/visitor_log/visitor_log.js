0ee89bba7ab6c2f596ac4b83d69c9abd367885c0
gangani9998 <gangani.9998@gmail.com>
2026-07-17 12:41:35 +0530

    feat: Auto-set valid_till to EOD on new record

--- visitor_management/visitor_management/doctype/visitor_log/visitor_log.js
+++ visitor_management/visitor_management/doctype/visitor_log/visitor_log.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2026, SFPL and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Visitor Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Visitor Log", {
+	onload: function(frm) {
+		// Auto-set "Valid Till" to End of Day if it is a new form
+		if (frm.is_new() && !frm.doc.valid_till) {
+			let today = frappe.datetime.get_today();
+			frm.set_value('valid_till', today + ' 23:59:59');
+		}
+	}
+});
